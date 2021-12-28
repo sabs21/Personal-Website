4f8f03002020c2203c20e3f70d6612da2695c1d2
sabs21 <sabian@my.ccsu.edu>
2021-12-28 14:19:59 -0500

    changed color palette

--- src/components/nav/NavLink.js
+++ src/components/nav/NavLink.js
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom"
 
 const NavLink = ({ text, to }) => {
     return (
-        <Link to={to} className="relative inline-flex justify-center items-center w-auto h-12 rounded-lg bg-orange-200">
-            <span className="relative inline-block text-xl text-gray-900 mx-2">{text}</span>
+        <Link to={to} className="relative inline-flex justify-center items-center w-auto h-12 rounded-lg hover:bg-cyan-200 hover:dark:bg-cyan-700 transition-colors">
+            <span className="relative inline-block text-xl text-cyan-900 dark:text-cyan-50 mx-2">{text}</span>
         </Link>
     )
 }
--- src/pages/Minecraft.js
+++ src/pages/Minecraft.js
@@ -2,8 +2,11 @@ import React from 'react'
 
 const Minecraft = () => {
     return (
-        <div>
-            <h1 className="text-5xl text-red-800">Some Test Text</h1>
+        <div className="px-8">
+            <h1 className="text-5xl text-sky-500 mt-4 mb-2">Witcher's Modpack</h1>
+            <p className="relative block text-gray-800 dark:text-gray-200">
+                Handcrafted for a vanilla plus experience. Dive into new dimensions, build bustling factories, and fight bosses to become an unstoppable force. This pack is optimized for multiplayer servers and is updated frequently, so be sure to check back here for any updates.
+            </p>
         </div>
     )
 }
