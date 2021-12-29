3b000cf7f682b5237f55ef7c43bf9e85d49df6ef
sabs21 <sabian@my.ccsu.edu>
2021-12-29 11:23:25 -0500

    added react-three-fiber and created 3d cubes

--- src/pages/Minecraft.js
+++ src/pages/Minecraft.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import MinecraftBlock from "../components/MinecraftBlock.js"
 
 const Minecraft = () => {
     return (
         <div className="px-8">
+            <MinecraftBlock />
             <h1 className="text-5xl text-sky-500 mt-4 mb-2">Witcher's Modpack</h1>
             <p className="relative block text-gray-800 dark:text-gray-200">
                 Handcrafted for a vanilla plus experience. Dive into new dimensions, build bustling factories, and fight bosses to become an unstoppable force. This pack is optimized for multiplayer servers and is updated frequently, so be sure to check back here for any updates.
